b3bf52ef2b69565ef2069c8c9ca8074b09daa4d5
cclerget <cedric.clerget@gmail.com>
2020-08-19 18:13:27 +0200

    Fix multi-arch docker images build/push

--- build/mage/targets/build_target.go
+++ build/mage/targets/build_target.go
@@ -7,6 +7,26 @@ import (
 	"github.com/ctrl-cmd/gobuild"
 )
 
+func ArchFromDockerPlatform(platform string) string {
+	switch platform {
+	case "linux/amd64":
+		return "amd64"
+	case "linux/386":
+		return "386"
+	case "linux/arm/v6":
+		return "arm"
+	case "linux/arm/v7":
+		return "arm"
+	case "linux/arm64":
+		return "arm64"
+	case "linux/ppc64le":
+		return "ppc64le"
+	case "linux/s390x":
+		return "s390x"
+	}
+	return ""
+}
+
 // ldFlags returns linker flags passed to Go command.
 func ldFlags() string {
 	flags := []string{
@@ -20,8 +40,8 @@ func ldFlags() string {
 func Install() error {
 	// for static build
 	os.Setenv("CGO_ENABLED", "0")
-	if os.Getenv("BUILD_GOARCH") != "" {
-		os.Setenv("GOARCH", os.Getenv("BUILD_GOARCH"))
+	if os.Getenv("TARGETPLATFORM") != "" {
+		os.Setenv("GOARCH", ArchFromDockerPlatform(os.Getenv("TARGETPLATFORM")))
 	}
 	return gobuild.RunInstall("-ldflags", ldFlags(), "./cmd/spks/")
 }
@@ -30,8 +50,8 @@ func Install() error {
 func Build() error {
 	// for static build
 	os.Setenv("CGO_ENABLED", "0")
-	if os.Getenv("BUILD_GOARCH") != "" {
-		os.Setenv("GOARCH", os.Getenv("BUILD_GOARCH"))
+	if os.Getenv("TARGETPLATFORM") != "" {
+		os.Setenv("GOARCH", ArchFromDockerPlatform(os.Getenv("TARGETPLATFORM")))
 	}
 	return gobuild.RunBuild("-o", "./build/spks", "-ldflags", ldFlags(), "./cmd/spks/")
 }
